547a2eb517146ff8f104e80f2acff41f42c58732
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-12-02 13:32:25 +0600

    Added global mongoose connection and promise types

---
+++ types/global.d.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var mongoose: {
+    conn: mongoose.Mongoose | null;
+    promise: Promise<mongoose.Mongoose> | null;
+  };
+}
+
+export {};

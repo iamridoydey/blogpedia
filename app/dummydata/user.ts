8de9681649015fcc51065ed916c4d7f5f07a84f8
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-12-11 07:30:30 +0600

    Added dummy data to avoid null and undefined value for users

---
+++ app/dummydata/user.ts
@@ -0,0 +1,15 @@
+export const user = {
+  firstname: "",
+  lastname: "",
+  email: "",
+  username: "",
+  profilePic: "",
+  coverpic: "",
+  occupation: "",
+  password: "",
+  isVerified: "",
+  followers: [],
+  following: [],
+  socialAccounts: [{}],
+  authProvider: ""
+}

79fb04a0582b17775cc80aabaab9504dd522c0c0
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-12-12 21:45:21 +0600

    Fixed Navbar ui

--- app/components/ui/navigation/Navbar.tsx
+++ app/components/ui/navigation/Navbar.tsx
@@ -4,22 +4,39 @@ import AuthNavbar from "./AuthNavbar";
 import NewUserNavbar from "./NewUserNavbar";
 import LimitedNavbar from "./LimitedNavbar";
 import { usePathname } from "next/navigation";
+import { useRef, useEffect, useState } from "react";
 
 export default function Navbar() {
   const pathname = usePathname();
-  // Check the current route
   const isLimitedNav =
     pathname.startsWith("/me") || pathname.startsWith("/blog");
-  // Check whether user is logged in or not
   const { data: session } = useSession();
+  const navRef = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState(0);
 
-  return session ? (
-    isLimitedNav ? (
-      <LimitedNavbar />
-    ) : (
-      <AuthNavbar />
-    )
-  ) : (
-    <NewUserNavbar />
+  useEffect(() => {
+    if (navRef.current) {
+      setHeight(navRef.current.offsetHeight);
+    }
+  }, [navRef]);
+
+  return (
+    <section className="h-full w-full">
+      <div
+        ref={navRef}
+        className="fixed top-0 left-0 right-0 w-full z-50 bg-white shadow-sm"
+      >
+        {session ? (
+          isLimitedNav ? (
+            <LimitedNavbar />
+          ) : (
+            <AuthNavbar />
+          )
+        ) : (
+          <NewUserNavbar />
+        )}
+      </div>
+      <div style={{ height: `${height}px`, backgroundColor: "#c6cade" }}></div>
+    </section>
   );
 }

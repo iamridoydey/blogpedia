1bccfd57d21c31670e8274464bd2c0a22b79ca30
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-12-13 22:04:42 +0600

    All the configuration files

---
+++ firebaseConfig.ts
@@ -0,0 +1,21 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAi5LjOLUE5XMVYVaVPm4EZUIbXaLVYPz0",
+  authDomain: "blogpedia21.firebaseapp.com",
+  projectId: "blogpedia21",
+  storageBucket: "blogpedia21.firebasestorage.app",
+  messagingSenderId: "367326244492",
+  appId: "1:367326244492:web:5d4db01e58962fc44615a9",
+  measurementId: "G-3TMZK2RKV8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const storage = getStorage(app);
+
+export { storage, analytics };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,24 @@ export default {
   ],
   theme: {
     extend: {
+      screens:{
+        "emini": "300",
+        "mini": "420px",
+        "esm": "480px",
+        "slg":"900px",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        bp_primary: "#0a66c2",
+      },
+      animation: {
+        "spin-slow": "spin-slow 1s infinite steps(12)",
+      },
+      keyframes: {
+        "spin-slow": {
+          "100%": { transform: "rotate(1turn)" },
+        },
       },
     },
   },

53ee608b82f9b03e1b5f40319c4aeeee00548c45
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-12-13 22:03:56 +0600

    Next-auth authentication extended types

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,18 +1,26 @@
-import { Types } from "mongoose";
+// import { Types } from "mongoose";
 import { DefaultSession, DefaultUser } from "next-auth";
+import { DefaultJWT } from "next-auth/jwt";
 declare module "next-auth" {
   interface User extends DefaultUser {
     id: string;
-    firstname: string;
-    lastname: string;
-    username: string;
-    profilepic?: string;
-    occupation: string;
-    followers: Types.ObjectId[];
-    following: Types.ObjectId[];
+    // firstname: string;
+    // lastname: string;
+    // username: string;
+    // profilepic?: string;
+    // occupation: string;
+    // isVerified: boolean;
+    // followers: Types.ObjectId[];
+    // following: Types.ObjectId[];
+    // authProvider: string;
   }
 
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    id?: string; // Optional ID for the token
+  }
+}
   interface Session extends DefaultSession {
-    user: User;
+    user: User
   }
 }

cda9849cb1775982e5272c76ded87ee8eb854af6
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-12-13 21:59:02 +0600

    Added few restriction

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function middleware(req:any) {
+export async function middleware(req: any) {
   const { pathname } = new URL(req.url); // Extract the pathname from the URL
 
   // Check whether the user has a valid token
@@ -14,14 +14,19 @@ export async function middleware(req:any) {
   // If no token and accessing protected routes, redirect to home page
   if (
     !token &&
-    (pathname.startsWith("/settings") || pathname.startsWith("/feed"))
+    (pathname.startsWith("/feed") || pathname.startsWith("/user") || pathname.startsWith("/me"))
   ) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
+
   // If token exists and accessing the home page, redirect to the feed page
-  if (token && pathname === "/") {
+  if (token && (pathname === "/" || pathname.startsWith("/auth/"))) {
     return NextResponse.redirect(new URL("/feed", req.url));
+  }
+
+  if (token && pathname === "/me/settings"){
+    return NextResponse.redirect(new URL("/me/settings/basicinfo", req.url));
   }
 
   // Allow the request to proceed
@@ -29,5 +34,5 @@ export async function middleware(req:any) {
 }
 
 export const config = {
-  matcher: ["/settings/:path*", "/feed/:path*", "/"], // Apply middleware to these routes
+  matcher: ["/feed/:path*", "/auth/:path*", "/user/:path*", "/me/:path*", "/"],
 };

a11d4f021271ab643e2aa4859cfc3ae4ffc494c2
Ridoy Chandra Dey <iamridoydey@gmail.com>
2024-12-14 08:14:41 +0600

    Alter params from auth->module->route pages

--- app/auth/[module]/page.tsx
+++ app/auth/[module]/page.tsx
@@ -1,16 +1,16 @@
 import SignIn from "@/app/components/auth/SignIn";
 import SignUp from "@/app/components/auth/SignUp";
 
-export default async function AuthPage({params}: {params: {module: string}}) {
+interface Params {
+  module: string;
+}
 
-  // params in dynamic routes need to use async
-  const {module} = await params;
+export default function AuthPage({ params }: { params: Params }) {
+  const { module } = params;
+
   return (
-    <div className="wrapper ">
-      {
-        module == "signin"? <SignIn/> : <SignUp/>
-      }
-      
+    <div className="wrapper">
+      {module === "signin" ? <SignIn /> : <SignUp />}
     </div>
   );
 }
